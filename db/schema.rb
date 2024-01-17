49d23d9483e96d5aaa11afbdc721a9c3d2a86024
jonathanoliveirasilva9 <jonathanoliveirasilva9@gmail.com>
2024-01-17 14:47:15 -0300

    create subcategories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_17_174614) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_174701) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_17_174614) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subcategories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
